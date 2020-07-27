41ff2a6eab76cb536f866a2e635bfa7fd9af8bd3
Mike Earley <mike@5sn1.com>
2020-07-27 15:27:25 -0400

    updating during lesson 9

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,67 +1,71 @@
+const pkg = require('./package')
 
-export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
+module.exports = {
   mode: 'universal',
+
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
-  target: 'server',
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    title: pkg.name,
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
+
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#39b982'
+  },
+
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
+   ** Global CSS
+   */
+  css: [],
+
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
-  components: true,
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    '@nuxtjs/axios'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {},
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
+   ** Build configuration
+   */
   build: {
+    /*
+     ** You can extend webpack config here
+     */
+
   }
 }
---
+++ pages/event/_id.vue
@@ -0,0 +1,36 @@
+    <template>
+  <div>
+    <h1>{{ event.title }}</h1>
+  </div>
+</template>
+    <script>
+import EventService from "@/services/EventService.js";
+
+export default {
+  head() {
+    return {
+      title: this.event.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "What you need to know about " + this.event.title,
+        },
+      ],
+    };
+  },
+  async asyncData({ error, params }) {
+    try {
+      const { data } = await EventService.getEvent(params.id);
+      return {
+        event: data,
+      };
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: "Unable to fetch event #" + params.id,
+      });
+    }
+  },
+};
+</script>
